83d003f779dcc0e893b50270d41a855afa9095aa
Matthew Starcher <mlstarcher@gmail.com>
2022-06-04 17:22:49 -0700

    Completed refactor of navigation based on login status

--- contexts/AuthenticatedUserProvider.js
+++ contexts/AuthenticatedUserProvider.js
@@ -1,16 +1,59 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext, useEffect, useContext } from 'react';
 import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { auth } from '../config/firebase';
 
 
 export const AuthenticatedUserContext = createContext({});
 
+export function useAuth() {
+  return useContext(AuthenticatedUserContext);
+}
+
 export const AuthenticatedUserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true)
 
+  function signup(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password).then(cred => {
+      const docRef = doc(db, 'users', cred.user.uid)
+      const payload = {
+        email: email,
+      }
+      setDoc(docRef, payload);
+      setLoading(false);
+    })
+    .catch(err => { console.log(err); })
+  }
 
+  function login(email, password) {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  function logOut() {
+    return signOut(auth);
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, currentUser => {
+      setUser(currentUser)
+      setLoading(false)
+    })
+    console.log('user: ', JSON.stringify(user))
+    return unsubscribe;
+  }, [])
+
+  const value = {
+    user,
+    setUser,
+    signup,
+    login,
+    logOut
+  }
+
+  console.log('AuthenticatedUserContext')
 
   return (
-    <AuthenticatedUserContext.Provider value={{ user, setUser }}>
+    <AuthenticatedUserContext.Provider value={{ value }}>
       {children}
     </AuthenticatedUserContext.Provider>
   );
--- navigation/RootNavigator.js
+++ navigation/RootNavigator.js
@@ -1,43 +1,19 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { View, ActivityIndicator, Text } from 'react-native';
 import { onAuthStateChanged } from 'firebase/auth';
 
 import { auth } from '../config/firebase';
-import { AuthenticatedUserContext } from '../contexts/AuthenticatedUserProvider';
+import { useAuth } from '../contexts/AuthenticatedUserProvider';
 import AuthStack from '../screens/AuthStack';
 import HomeStack from './HomeStack';
 
 export default function RootNavigator() {
-  const { user, setUser } = useContext(AuthenticatedUserContext);
-  const [isLoading, setIsLoading] = useState(true);
+  const { user, setUser, loading } = useAuth();
+  // const [isLoading, setIsLoading] = useState(true);
   console.log('RootNavigator')
-  // useEffect(() => {
-    // onAuthStateChanged returns an unsubscriber
-  //   const unsubscribeAuth = auth.onAuthStateChanged(async authenticatedUser => {
-  //     try {
-  //       await (authenticatedUser ? setUser(authenticatedUser) : setUser(null));
-  //       setIsLoading(false);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   });
 
-  //   // unsubscribe auth listener on unmount
-  //   return unsubscribeAuth;
-  // }, []);
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, user => {
-      setUser(user)
-      setIsLoading(false)
-    })
-    console.log('user:', user)
-    return unsubscribe;
-  }, [])
-
-
-  if (isLoading) {
+  if (loading) {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <ActivityIndicator size='large' />
